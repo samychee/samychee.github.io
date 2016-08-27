25c05d7f6dbbd417c55c423b73bd2663a19a94e7
Raymond Chee <rjchee@gmail.com>
2016-08-26 20:50:46 -0500

    dynamic webpage

--- gchee.py
+++ gchee.py
@@ -1,26 +1,51 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
+
+
+def init_config(app):
+    # set up json config
+    app.config.from_json('data.json')
+    categories = dict()
+    num_images = len(app.config['IMAGES'])
+    # group together categories
+    for image in app.config['IMAGES']:
+        app.config[image['FILENAME']] = image
+        if image['CATEGORY'] not in categories:
+            categories[image['CATEGORY']] = []
+        categories[image['CATEGORY']].append(image)
+    # sort categories by decreasing number of pictures in category, then by alphabetical order
+    category_image_list = app.config['categories'] = sorted(categories.items(), key=lambda item: (-len(item[1]), item[0]))
+    flat_list = app.config['IMAGES'] = [image for tup in category_image_list for image in tup[1]]
+    for idx, image in enumerate(flat_list):
+        image['back'] = flat_list[idx - 1]
+        image['forward'] = flat_list[(idx + 1) % num_images]
+
+    # set up freezer configs
+    app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html'
+    app.config['FREEZER_RELATIVE_URLS'] = True
+
 
 app = Flask(__name__)
-app.config.from_json('data.json')
-for idx, image in enumerate(app.config['IMAGES']):
-    app.config[image['FILENAME']] = image
-    image['back'] = app.config['IMAGES'][idx - 1]
-    image['forward'] = app.config['IMAGES'][(idx + 1) % len(app.config['IMAGES'])]
+init_config(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html', images=app.config['IMAGES'])
+    return render_template('portfolio.html', categories=app.config['categories'])
+
 
 @app.route('/images/<name>')
 def image_page(name=None):
     if name is None:
         return portfolio()
     return render_template('artpage.html', image=app.config[name])
+
 
 @app.route('/about')
 def about():
---
+++ static/javascript/artpage.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var back_button = document.getElementsByClassName('carousel-button-back');
+    var forward_button = document.getElementsByClassName('carousel-button-forward');
+    if (back_button.length) {
+        back_button = back_button[0];
+    } else {
+        back_button = null;
+    }
+    if (forward_button.length) {
+        forward_button = forward_button[0];
+    } else {
+        forward_button = null;
+    }
+    if (back_button || forward_button) {
+        document.onkeydown = function(e) {
+            e = e || window.event;
+            switch (e.keyCode) {
+                case 37:
+                    if (back_button) {
+                        window.location.href = back_button.href;
+                    }
+                    break;
+                case 39:
+                    if (forward_button) {
+                        window.location.href = forward_button.href;
+                    }
+                    break;
+            }
+        }
+    }
+});
